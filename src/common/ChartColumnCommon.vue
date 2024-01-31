784d80280d056960590d8d5ec255dd65e51bcc3c
NGUYENCONGTHANG <congthang25092003@gmail.com>
2024-01-31 14:46:50 +0700

    update code

--- src/common/ChartColumnCommon.vue
+++ src/common/ChartColumnCommon.vue
@@ -1,5 +1,16 @@
 <template>
-  <Bar :data="data" :options="options" />
+  <div>
+    <h1>{{ title }}</h1>
+    <div
+      class="chart-common"
+      :style="{
+        width: String(stylesCommon.width),
+        marginTop: String(stylesCommon.marginTop),
+      }"
+    >
+      <Bar :data="data" :options="options" :height="stylesCommon.height" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -13,6 +24,7 @@ import {
   BarElement,
   CategoryScale,
   LinearScale,
+  ChartOptions,
 } from "chart.js";
 
 ChartJS.register(
@@ -34,6 +46,13 @@ interface ChartData {
   datasets: any;
 }
 
+interface StyleList {
+  height?: String;
+  width?: String;
+  marginTop?: String;
+  marginBottom?: String;
+}
+
 export default defineComponent({
   name: "ChartColumnCommon",
   components: {
@@ -44,34 +63,112 @@ export default defineComponent({
       type: Array as () => Array<ItemList>,
       required: true,
     },
+    styles: {
+      type: Object as () => StyleList,
+      default: () => ({}),
+    },
+    titleValue: String,
+    noteX: String,
+    noteY: String,
+    listLabelData: {
+      type: Array as () => Array<String>,
+      required: true,
+    },
+    listColorData: {
+      type: Array as () => Array<String>,
+      required: true,
+    },
   },
-  data(): { data: ChartData; options: { responsive: boolean } } {
+  data(): {
+    data: ChartData;
+    options: ChartOptions;
+    title: String;
+    stylesCommon: StyleList;
+  } {
+    const datasets = this.listLabelData.map((label) => ({
+      label: label,
+      backgroundColor: "",
+      data: [],
+    }));
+
     return {
       data: {
-        labels: ["January", "February", "March"],
-        datasets: [{ data: [40, 20, 12] }],
+        labels: [],
+        datasets: datasets,
       },
       options: {
         responsive: true,
-      },
+        scales: {
+          x: {
+            stacked: true,
+            title: {
+              display: true,
+              text: this.noteX,
+            },
+          },
+          y: {
+            stacked: true,
+            title: {
+              display: true,
+              text: this.noteY,
+            },
+          },
+        },
+      } as ChartOptions,
+      stylesCommon: {},
+      title: "",
     };
   },
   watch: {
     chartList: {
       handler(newList) {
-        // Update labels and datasets when chartList changes
         this.data.labels = newList.map((item: ItemList) => item.label);
-        this.data.datasets[0].data = newList.map(
-          (item: ItemList) => item.dataset
+
+        this.data.datasets = this.listLabelData.map((label, index) => ({
+          label: label,
+          backgroundColor: "",
+          data: newList.map((item: ItemList) => item.dataset[index]),
+        }));
+      },
+      immediate: true,
+    },
+    listLabelData: {
+      handler(newList) {
+        const numLabels = Math.min(newList.length, this.data.datasets.length);
+
+        for (let i = 0; i < numLabels; i++) {
+          this.data.datasets[i].label = newList[i];
+        }
+      },
+      immediate: true,
+    },
+    listColorData: {
+      handler(newList) {
+        const numBackgroundColors = Math.min(
+          newList.length,
+          this.data.datasets.length
         );
 
-        // Log updated data to console (optional)
-        console.log(this.data.labels);
-        console.log(this.data.datasets);
+        for (let i = 0; i < numBackgroundColors; i++) {
+          this.data.datasets[i].backgroundColor = newList[i];
+        }
       },
       immediate: true,
     },
   },
+  created() {
+    if (this.titleValue !== undefined) {
+      this.title = this.titleValue;
+    }
+    this.stylesCommon = this.styles;
+  },
   methods: {},
 });
 </script>
+
+<style>
+.chart-common {
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
--- src/views/ChartDemo.vue
+++ src/views/ChartDemo.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="chart-demo">
-    <h1>This is Chart Demo</h1>
-    <ChartColumnCommon :chartList="listData" />
+    <ChartColumnCommon
+      :chartList="listData"
+      :titleValue="title"
+      :styles="styles"
+      :noteX="noteX"
+      :noteY="noteY"
+      :listLabelData="listLabelData"
+      :listColorData="listColorData"
+    />
   </div>
 </template>
 
@@ -16,33 +23,45 @@ export default defineComponent({
   },
   data() {
     return {
+      listLabelData: ["Điểm toán", "Điểm hóa", "Điểm Văn"],
+      listColorData: ["#f87979", "rgb(21, 106, 155)", "rgb(214, 99, 53)"],
       listData: [
         {
-          label: "January",
-          dataset: 40,
+          label: "Sơn",
+          dataset: [8, 5, 5],
         },
         {
-          label: "February",
-          dataset: 20,
+          label: "Vinh",
+          dataset: [10, 7, 6],
         },
         {
-          label: "March",
-          dataset: 12,
+          label: "Hạnh",
+          dataset: [7, 7, 8],
         },
         {
-          label: "January",
-          dataset: 40,
+          label: "Thắng",
+          dataset: [4, 7, 7],
         },
         {
-          label: "February",
-          dataset: 20,
+          label: "Vinh",
+          dataset: [10, 7, 2],
         },
         {
-          label: "March",
-          dataset: 12,
+          label: "Hiệu",
+          dataset: [6, 7, 4],
         },
       ],
+      title: "Biểu đồ điểm toán và hóa của học sinh",
+      styles: {
+        width: "75%",
+        height: "100px",
+        marginTop: "100px",
+      },
+      noteX: "Họ và tên học sinh",
+      noteY: "Điểm (%)",
     };
   },
 });
 </script>
+<style>
+</style>
